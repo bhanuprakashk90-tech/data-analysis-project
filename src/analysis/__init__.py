0c6a11046248f0012c0225a2e386ce990e8dd1d3
KUNCHAM Bhanu Prakash <bhanuprakashkuncham@gmail.com>
2026-06-02 21:36:04 +0200

    Initialize analysis module

---
+++ src/analysis/__init__.py
@@ -0,0 +1,15 @@
+"""
+Data analysis module
+"""
+
+from .statistics import (
+    get_basic_stats,
+    get_correlation,
+    perform_anova
+)
+
+__all__ = [
+    'get_basic_stats',
+    'get_correlation',
+    'perform_anova'
+]
